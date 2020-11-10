2c40ba30de088c7d7ecf005dc2c840e65ec37265
Muntasir Sauvik <muntasir.sauvik@cpdist.ca>
2020-11-10 05:26:04 -0600

    Completed the Entire EJS section

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,48 +1,53 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
-// require('dotenv').config();
-
 const app = express();
 
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+const date = require(__dirname + "/date.js");
+
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
+
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
 
+  const day = date.getDate();
 
-  switch(currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Satruday";
-      break;
-    default:
-      console.log("Error: current day is equal to: "+ currentDay);
-  } 
-  res.render("list.ejs",{kindOfDay: day});
+  res.render("list.ejs", {
+    listTitle: day,
+    newListItems: items
+  });
 
 });
+
+app.post("/", function(req, res) {
+
+  const item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+      workItems.push(item);
+      res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+})
+
+app.post("/work", function(req, res) {
+  const item = req.body.newItem;
+  res.redirect("/work");
+})
+
+app.get("/about", function(req, res) {
+  res.render("about");
+})
 
 app.listen(3001, function(){
   console.log("Server started on port 3001");
